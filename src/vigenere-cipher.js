9afe8b4a2e4fbd6d33f911093e5f6363bef851a7
Marta Kozak <martakozak94666@gmail.com>
2020-02-25 17:13:08 +0200

    init: upload the vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,19 +1,53 @@
 class VigenereCipheringMachine {
-    constructor(direct = true, reverse){
-        this.direct = direct;
-        this.reverse = reverse;
+    constructor(bool = true) {
+        this.bool = bool;
     }
+
     encrypt(message, key) {
-        if(message == undefined || key == undefined ){
+        if (message === undefined || key === undefined) {
             throw new Error('Error');
         }
+        let result = [];
+        let t = 0;
+        for (let i = 0; i < message.length; i++) {
+            message = message.toUpperCase();
+            key = key.toUpperCase();
+            if((message[i].match(/[A-Z]/))) {
+                if (message.indexOf(message[i]) <  0) {
+                    result.push(String.fromCodePoint((message[i].charCodeAt(0) - 'A'.charCodeAt(0) + (26 - (key[t % key.length].charCodeAt(0) - 'A'.charCodeAt(0)))) % 26 + 'A'.charCodeAt(0)));
+                } else {
+                    result.push(String.fromCodePoint((message[i].charCodeAt(0) - 'A'.charCodeAt(0) + (key[t % key.length].charCodeAt(0) - 'A'.charCodeAt(0))) % 26 + 'A'.charCodeAt(0)));
+                }
+                t++;
+            } else {
+                result.push(message[i]);
+            }
+      }
+      return (this.bool === false) ? result.reverse().join('') : result.join('');
     }
 
     decrypt(encryptedMessage, key) {
-        if(encryptedMessage == undefined || key == undefined){
-            throw new Error('Error');  
-    }
-    }
+        if (encryptedMessage === undefined || key === undefined){
+             throw new Error('Error');
+    }   
+        let result = [];
+        let t = 0;
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            encryptedMessage = encryptedMessage.toUpperCase();
+            key = key.toUpperCase();
+            if((encryptedMessage[i].match(/[A-Z]/))) {
+                if (encryptedMessage.indexOf(encryptedMessage[i]) <  0) {
+                    result.push(String.fromCodePoint((encryptedMessage[i].charCodeAt(0) - 'A'.charCodeAt(0) + (key[t % key.length].charCodeAt(0) - 'A'.charCodeAt(0))) % 26 + 'A'.charCodeAt(0)));
+                } else {
+                    result.push(String.fromCodePoint((encryptedMessage[i].charCodeAt(0) - 'A'.charCodeAt(0) + (26 - (key[t % key.length].charCodeAt(0) - 'A'.charCodeAt(0)))) % 26 + 'A'.charCodeAt(0)));
+                }
+                t++;
+            } else {
+                result.push(encryptedMessage[i]);
+            }
+        }
+        return (this.bool === false) ? result.reverse().join('') : result.join('');
+      }
+        
 }
-
 module.exports = VigenereCipheringMachine;
